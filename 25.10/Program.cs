fe5fd960b808732fb0aea64ed6b4624075554bab
User <User@WIN-NOEOKDOM1AS>
2021-10-25 11:15:02 +1000

    Добавьте файлы проекта.

---
+++ 25.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число b");
+            int.TryParse(Console.ReadLine(), out int b);
+            Random rnd = new Random();
+            int a = rnd.Next(0, 100);
+            if (b>a)
+                Console.WriteLine("Много");
+            else
+                if (b<a)
+                Console.WriteLine("Мало");
+            else
+                Console.WriteLine("Угадал");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
